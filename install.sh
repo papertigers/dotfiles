def4dce9d1dc87805ef6276f03c25ad70e2fe726
Mike Zeller <mike@mikezeller.net>
2017-08-08 01:26:37 -0400

    iTerm2 settings

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@
 #
 # Modified: Mike Zeller <mike@mikezeller.net>
 
-iterm2_plist_path="~/Library/Preferences/com.googlecode.iterm2.plist"
+iterm2_profile="$PWD/osx/iTerm2"
+font_dir="$HOME/.local/share/fonts"
 
 # makes "defaults" command print to screen
 defaults() {
@@ -34,6 +35,7 @@ done
 
 # Mac OS X specific
 if [[ $(uname) == 'Darwin' ]]; then
+	font_dir="$HOME/Library/Fonts"
 	if ! brew help &> /dev/null; then
 		echo 'installing homebrew'
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -44,9 +46,25 @@ if [[ $(uname) == 'Darwin' ]]; then
 
 	brew install the_silver_searcher
 	brew install wget
+	brew install tmux
+fi
 
-	# This doesn't work yet
-	# iTerm2 preferences
-	#[[ -f $iterm2_plist_path ]] && rm "$iterm2_plist_path"
-	#symlink "$PWD/osx/iTerm2/com.googlecode.iterm2.plist" "$iterm2_plist_path"
+# Install custom fonts for powerline
+mkdir -p "$font_dir"
+echo 'Copying fonts'
+cp "fonts/Inconsolata-g for Powerline.otf" "$font_dir"
+if command -v fc-cache @>/dev/null ; then
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -f $font_dir
+fi
+
+# Mac OS X modifications
+if defaults read com.apple.finder &>/dev/null; then
+	echo 'modifying iTerm2 preference location'
+
+	# Specify the preferences directory
+	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$iterm2_profile"
+	# Tell iTerm2 to use the custom preferences in the directory
+	defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+	echo 'restart iTerm2 to pick up the new configuration'
 fi
